1eb14c1ba37735b1f9db8c2ae95862d167d78998
TheJuliuscanute <juliuscanute@touchcapture.net>
2019-04-14 10:52:25 +1000

    expose node port

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ db.connect((err) => {
 global.db = db;
 
 // configure middleware
-app.set('port', process.env.port || port); // set express to use this port
+app.set('port', process.env.APP_PORT || port); // set express to use this port
 app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
 app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
